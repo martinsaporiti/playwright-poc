540c755cb9fe35384baaab9ed9fa2f650a8d5bc3
martinsaporiti <martinsaporiti@gmail.com>
2024-08-08 09:07:45 -0300

    remove reportet

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  // reporter: 'html',
   use: {
     baseURL: 'https://issuer-node-api-v2-test.privado.id',//TODO get from container
     extraHTTPHeaders: {
